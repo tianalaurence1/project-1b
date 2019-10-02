d9c8c153c82e0da6fe708976d62899f14034ede2
Tdog <tianalaurence@gmail.com>
2019-10-01 18:55:34 -0700

    First commit

--- project-1b.py
+++ project-1b.py
@@ -1,6 +1,6 @@
 # Author: Tiana Laurence
 # Date: 09/21/2019
-# Description: Asks the user for their name and then greets them by their name.
+# Description: It asks the user for their name and then greets them by their name.
 
 your_name = input("Please enter your name: ")
 print("Hello " + your_name + "!")
